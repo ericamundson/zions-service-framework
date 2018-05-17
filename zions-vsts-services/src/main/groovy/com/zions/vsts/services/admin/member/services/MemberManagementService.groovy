43ecebfd4216ec912d75825b7ec4f4c26db5eace
z091182 <eric.amundson2@zionsbancorp.com>
2018-05-17 09:19:31 -0600

    Move new changes

---
+++ zions-vsts-services/src/main/groovy/com/zions/vsts/services/admin/member/services/MemberManagementService.groovy
@@ -0,0 +1,56 @@
+package com.zions.vsts.services.admin.member.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.zions.vsts.services.tfs.rest.GenericRestClient;
+
+import groovyx.net.http.ContentType
+
+@Component
+public class MemberManagementService {
+	@Autowired(required=true)
+	private GenericRestClient genericRestClient;
+
+	public MemberManagementService() {
+		
+	}
+	
+	public def addMember(String id, String role, String[] projects) {
+		return null;
+	
+	}
+	
+	public def getAllProjects(String collection) {
+		def query = ['api-version':'4.0']
+		def result = genericRestClient.get(
+			contentType: ContentType.JSON,
+			uri: "${genericRestClient.getTfsUrl()}/${collection}/_apis/projects",
+			headers: [Accept: 'application/json'],
+			query: query
+			)
+		return result
+
+	}
+	
+	public def getAllTeams(String collection) {
+		def projects = getAllProjects(collection)
+		def teams = [:]
+		def query = ['api-version':'4.0']
+		projects.value.each { project -> 
+			def name = project.name
+			def result = genericRestClient.get(
+				contentType: ContentType.JSON,
+				uri: "${genericRestClient.getTfsUrl()}/${collection}/_apis/projects/${name}/teams",
+				headers: [Accept: 'application/json'],
+				query: query
+				)
+			result.value.each { team ->
+				teams[team.name] = team
+				
+			}
+			
+		}
+		return teams
+	}
+}
