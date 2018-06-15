65fb7c9c367586b402c1c2dc31ed2a3a82e50219
z091182 <eric.amundson2@zionsbancorp.com>
2018-06-15 11:07:31 -0600

    Added xld services project

---
+++ zions-xld-services/src/main/groovy/com/zions/xld/services/rest/client/GenericRestClient.groovy
@@ -0,0 +1,128 @@
+package com.zions.xld.services.rest.client
+
+import groovy.json.JsonOutput
+import groovy.json.JsonSlurper
+import groovyx.net.http.HttpResponseDecorator
+import groovyx.net.http.RESTClient;
+import org.apache.http.auth.AuthScope
+import org.apache.http.auth.UsernamePasswordCredentials
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+@Component
+class GenericRestClient {
+	private RESTClient delegate;
+	
+	
+	String xldUrl;
+	
+	private String user;
+		
+	private String token
+
+	@Autowired
+	public GenericRestClient(@Value('${tfs.url}') String tfsUrl,
+		@Value('${tfs.user}') String user,
+		@Value('${tfs.token}') String token) {
+		this.xldUrl = tfsUrl
+		this.token = token;
+		this.user = user;
+		delegate = new RESTClient(tfsUrl)
+		delegate.ignoreSSLIssues()
+		delegate.handler.failure = { it }
+		setProxy()
+		setCredentials(user, token);
+
+	}
+	def setProxy() {
+		String proxyHost = System.getProperty("proxy.Host")
+		if (proxyHost != null) {
+			String proxyPort = System.getProperty("proxy.Port")
+			String proxyUser = System.getProperty("proxy.User")
+			String proxyPassword = System.getProperty("proxy.Password")
+			
+			delegate.client.getCredentialsProvider().setCredentials(
+				new AuthScope(proxyHost, Integer.parseInt(proxyPort)),
+				new UsernamePasswordCredentials(proxyUser, proxyPassword)
+			)
+			delegate.setProxy(proxyHost, Integer.parseInt(proxyPort), 'http')
+			
+		}
+	}
+	
+	void setCredentials(String user, String token) {
+		String auth = "$user:$token".bytes.encodeBase64()
+		delegate.headers['Authorization'] = 'Basic ' + auth
+		
+	}
+	
+	def get(Map input) {
+		Map oinput = checkBlankCollection(input)
+		HttpResponseDecorator resp = delegate.get(oinput)
+		JsonOutput t
+		def out = JsonOutput.prettyPrint(JsonOutput.toJson(resp.data))
+		if ("${out}" == 'null') return null
+		JsonSlurper sl = new JsonSlurper()
+		def oOut = sl.parseText(out)
+		return oOut;
+	}
+	
+	def put(Map input) {
+		Map oinput = checkBlankCollection(input)
+		HttpResponseDecorator resp = delegate.put(oinput)
+		
+		if (resp.status != 200) {
+			return null;
+		}
+		def out = JsonOutput.prettyPrint(JsonOutput.toJson(resp.data))
+		if ("${out}" == 'null') return null
+		JsonSlurper sl = new JsonSlurper()
+		def oOut = sl.parseText(out)
+		return oOut;
+	}
+	
+	def delete(Map input) {
+		Map oinput = checkBlankCollection(input)
+		HttpResponseDecorator resp = delegate.delete(oinput)
+		if (resp.status != 204) {
+			return null;
+		}
+	}
+	
+	def patch(Map input) {
+		Map oinput = checkBlankCollection(input)
+		HttpResponseDecorator resp = delegate.patch(oinput)
+		
+		if (resp.status != 200) {
+			return null;
+		}
+		def out = JsonOutput.prettyPrint(JsonOutput.toJson(resp.data))
+		if ("${out}" == 'null') return null
+		JsonSlurper sl = new JsonSlurper()
+		def oOut = sl.parseText(out)
+		return oOut;
+	}
+
+	def post(Map input) {
+		Map oinput = checkBlankCollection(input)
+		HttpResponseDecorator resp = delegate.post(oinput)
+		JsonOutput t
+		def out = JsonOutput.prettyPrint(JsonOutput.toJson(resp.data))
+		if ("${out}" == 'null') return null
+		JsonSlurper sl = new JsonSlurper()
+		def oOut = sl.parseText(out)
+		return oOut;
+	}
+	
+	def checkBlankCollection(Map input) {
+		String uri = "${input.uri}"
+		String checkedUri = "${this.xldUrl}//"
+		if (uri.startsWith(checkedUri) ) {
+		
+			uri = "${this.xldUrl}/${uri.substring(checkedUri.length())}"
+			input.uri = uri
+		}
+		return input
+	}
+}
