7bf9ec9bb420ed80d00081d66d2d46e4ce7fc548
z070187 <michael.angelastro@zionsbancorp.com>
2019-02-20 13:03:00 -0700

    created zions-content-microservice sub project and added 3 classes

---
+++ zions-content-microservice/src/main/groovy/com/zions/content/services/AppConfig.groovy
@@ -0,0 +1,21 @@
+package com.zions.content.services
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+import com.zions.common.services.cli.action.CliAction
+
+@Configuration
+@ComponentScan("com.zions.vsts.services")
+public class AppConfig {
+	Map<String, CliAction> actions;
+	
+	@Bean
+	public Map<String, CliAction> actionsMap() {
+		if (actions == null)
+			actions = [:];
+		return actions;
+	}
+}
