0ebb1ddd52671154aba36415c6ebc53c2737e081
z091182 <Eric.Amundson2@zionsbancorp.com>
2019-03-12 03:20:56 -0600

    Updates for rollup
    #86009 #78450

--- zions-vsts-cli/src/main/groovy/com/zions/vsts/services/cli/action/wit/ExportWIT.groovy
+++ zions-vsts-cli/src/main/groovy/com/zions/vsts/services/cli/action/wit/ExportWIT.groovy
@@ -25,70 +25,35 @@ class ExportWIT implements CliAction {
 			collection = data.getOptionValues('tfs.collection')[0]
 		} catch (e) {}
 		String project = data.getOptionValues('tfs.project')[0]
-		String workItemName = data.getOptionValues('tfs.workitem.name')[0]
+		String workItemName = data.getOptionValues('tfs.workitem.names')[0]
 		if ("${workItemName}" == 'all') {
 			def wits = processTemplateService.getWorkItemTypes(collection, project)
 			wits.value.each { wit -> 
-				
-				def witData = processTemplateService.getWorkitemTemplateFields(collection, project, "${wit.name}")
-				def writer = new StringWriter()
-				MarkupBuilder bXml = new MarkupBuilder(writer)
-				bXml.'witd:WITD'(application:'Work item type editor',
-					version: '1.0',
-					'xmlns:witd': 'http://schemas.microsoft.com/VisualStudio/2008/workitemtracking/typedef') {
-					WORKITEMTYPE(name: "${wit.name}") {
-						DESCRIPTION("general work item starter")
-						FIELDS {
-							witData.value.each { field ->
-								def fieldDetails = processTemplateService.getField(collection, project, field.referenceName)
-								if (fieldDetails != null) {
-									FIELD(name: "${field.name}", refname: "${field.referenceName}", type: "${fieldDetails.type}".trim(), dimension: 'reportable') {
-										HELPTEXT "${field.helpText}"
-										if (field.allowedValues.size() > 0) {
-											ALLOWEDVALUES(expanditems: true) {
-												field.allowedValues.each { value ->
-													LISTITEM(value: "${value}")
-												}
-											}
-										}
-									}
-								} else {
-									FIELD(name: "${field.name}", refname: "${field.referenceName}", type: "${field.referenceName}", dimension: 'reportable') {
-										HELPTEXT "${field.helpText}"
-										if (field.allowedValues.size() > 0) {
-											ALLOWEDVALUES(expanditems: true) {
-												field.allowedValues.each { value ->
-													LISTITEM(value: "${value}")
-												}
-											}
-										}
-									}
-								}
-							}
-					
-						}
-					}
-				}
-				def outFile = data.getOptionValues('template.dir')[0];
-				File oFile = new File("${outFile}/${wit.name}.xml");
+				def witChanges = processTemplateService.translateWitChanges(collection, project, "${wit.name}")
+				def outFile = data.getOptionValues('export.dir')[0];
+				File oFile = new File("${outFile}/${wit.name}.json");
 				def w = oFile.newWriter();
-				w << writer.toString()
+				w << new JsonBuilder(witChanges).toPrettyString()
 				w.close();
 	
 			}
 		} else {
-			def xml = processTemplateService.getWorkitemTemplate(collection, project, workItemName)
-			def outFile = data.getOptionValues('template.dir')[0];
-			File oFile = new File("${outFile}/${workItemName}.xml");
-			def w = oFile.newWriter();
-			w << "${xml}"
-			w.close();
+			String[] witNames = data.getOptionValues('tfs.workitem.names')[0].split(',')
+			witNames.each { name -> 
+				def witChanges = processTemplateService.translateWitChanges(collection, project, "${name}")
+				def outFile = data.getOptionValues('export.dir')[0];
+				File oFile = new File("${outFile}/${name}.json");
+				def w = oFile.newWriter();
+				w << new JsonBuilder(witChanges).toPrettyString()
+				w.close();
+			}
 		}
 		return null;
 	}
+	
 
 	public Object validate(ApplicationArguments args) throws Exception {
-		def required = ['tfs.url', 'tfs.user', 'tfs.token', 'tfs.project', 'tfs.workitem.name','template.dir']
+		def required = ['tfs.url', 'tfs.user', 'tfs.token', 'tfs.project', 'tfs.workitem.names','export.dir']
 		required.each { name ->
 			if (!args.containsOption(name)) {
 				throw new Exception("Missing required argument:  ${name}")
---
+++ zions-vsts-websocket-microservice/src/test/groovy/com/zions/vsts/services/AppConfigTest.groovy
@@ -0,0 +1,14 @@
+package com.zions.vsts.services;
+
+import static org.junit.Assert.*
+
+import org.junit.Test
+
+import spock.lang.Specification
+
+class AppConfigTest extends Specification {
+
+	public void 'Initial test'() {
+	}
+
+}
