fe343eeee80cdef477584f4beb008b29923e5dae
z091182 <Eric.Amundson2@zionsbancorp.com>
2019-04-24 06:55:51 -0600

    try again
    #126497

--- zions-common/src/test/groovy/com/zions/common/services/cache/MongoDBCacheManagementServiceSpec.groovy
+++ zions-common/src/test/groovy/com/zions/common/services/cache/MongoDBCacheManagementServiceSpec.groovy
@@ -76,7 +76,7 @@ public class MongoDBCacheManagementServiceSpec extends Specification {
 }
 
 @TestConfiguration
-@Profile("integration-test")
+@Profile("test")
 @ComponentScan(["com.zions.common.services.test", "com.zions.common.services.cache.db"])
 @PropertySource("classpath:test.properties")
 @EnableMongoRepositories(basePackages = "com.zions.common.services.cache.db")
@@ -97,6 +97,7 @@ class MongoDBCacheManagementServiceTestConfig {
 		
 		return new EmbeddedMongoBuilder()
 			.version('3.2.16')
+			.tempDir('build/tmp')
 			.downloadPath('file:./../zions-common-data/mongodb/')
 			.artifactStorePath('./build/embeddeddb')
 			.bindIp("127.0.0.1")
