8b1424fa261a0ac53976f69939007c1503216f5d
z091182 <Eric.Amundson2@zionsbancorp.com>
2019-05-09 11:55:18 -0600

    Enable RM SQL queries
    #126498

---
+++ zions-common/src/main/groovy/com/zions/common/services/db/DatabaseQueryService.groovy
@@ -0,0 +1,95 @@
+package com.zions.common.services.db
+
+import groovy.sql.Sql
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+
+/**
+ * Concrete Database query service.
+ * 
+ * @author z091182
+ *
+ */
+@Component
+class DatabaseQueryService implements IDatabaseQueryService {
+	@Value('${db.url:}')
+	String dbUrl
+	
+	@Value('${db.user:}')
+	String dbUser
+	
+	@Value('${db.password:}')
+	String dbPassword
+	
+	@Value('${db.driver:oracle.jdbc.datasource.OracleDataSource}')
+	String dbDriver
+
+	@Value('${page.size:100}')
+	int pageSize
+	
+	Sql sql = null
+	
+	def columnNames = []
+	
+	def index = 1
+	
+	String select
+	
+	def init() {
+		if (sql) return
+		sql = groovy.sql.Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
+	}
+	
+	def metaCB = { metaData -> 
+		int c = metaData.columnCount 
+		this.columnNames.clear()
+		
+		for (int i = 0; i<c; i++) {      
+			this.columnNames.add(metaData.getColumnLabel(i+1))
+		}
+	}
+	
+	@Override
+	public def query(String select) {
+		init()
+		this.select = select
+		index = 1
+		def page = []
+		sql.eachRow(this.select,metaCB, index, pageSize) { row ->
+			def rowm = [:]
+			columnNames.each { name ->
+				String oname = "${name}".toLowerCase()
+				rowm[oname] = row."${name}"
+			}
+			page.add(rowm)
+		}
+		return page;
+	}
+
+	@Override
+	public def nextPage() {
+		def page = []
+		sql.eachRow(this.select, index, pageSize) { row ->
+			def rowm = [:]
+			columnNames.each { name ->
+				String oname = "${name}".toLowerCase()
+				rowm[oname] = row."${name}"
+			}
+			page.add(rowm)
+		}
+		if (page.size() == 0) return null
+		return page;
+	}
+	
+	public String pageUrl() {
+		index += pageSize
+		return "${select}/${index}/${pageSize}"
+	}
+
+	@Override
+	public String initialUrl() {
+		// TODO Auto-generated method stub
+		return "${select}/1/${pageSize}"
+	}
+
+}
