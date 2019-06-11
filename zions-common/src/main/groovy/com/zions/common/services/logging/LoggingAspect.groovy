8b665ed9249a7a8e253296b87bdd383280747ad2
z070187 <michael.angelastro@zionsbancorp.com>
2019-06-11 12:05:09 -0600

    reverted to previous install to fix eclipse issues
    updating logging code to include If/else logic
    
    for when log member object is found/not found

--- zions-common/src/main/groovy/com/zions/common/services/logging/LoggingAspect.groovy
+++ zions-common/src/main/groovy/com/zions/common/services/logging/LoggingAspect.groovy
@@ -8,8 +8,8 @@ import org.aspectj.lang.annotation.Aspect;
 import org.springframework.context.annotation.Configuration
 
 /**
- * Handles method timing logs.  
- * 
+ * Handles method timing logs.
+ *
  * @author Michael Angelastro
  *
  */
@@ -17,15 +17,15 @@ import org.springframework.context.annotation.Configuration
 @Slf4j
 public class LoggingAspect {
 
-	/* Created by Michael Angelastro
+	/* Created by
 	 * 05/29/2019 for Zions Service Framework Logging
-	 * 
+	 *
 	 * This is aLogging Aspect for the Loggable annotation that calculates method runtimes
 	 * for all methods under classes annotated with @Loggable*/
 
 	/**
 	 * Logs execution time of method under aspect.
-	 * 
+	 *
 	 * @param joinPoint - method under join
 	 * @return actual return of method under join point.
 	 * @throws Throwable
@@ -34,21 +34,33 @@ public class LoggingAspect {
 	public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
 
 		def obj = joinPoint.this
-		Logger iLog = log
-		long start = System.currentTimeMillis();
 		Object proceed = joinPoint.proceed();
-		long executionTime = System.currentTimeMillis() - start;
+		Logger iLog = log
 
+		
 		try {
-
-			/*First statement of try block attempts to test if log members exist on object.
-			 If it does, then iLog will get set to incoming object's logger*/
-
-			obj.log.isInfoEnabled()
+			
+			/*First statement of try block attempts to test if log member exists on object.
+			If it does, then iLog will get set to incoming object's logger*/
+			
+			if(log.isInfoEnabled()){
 			iLog = obj.log
-		} catch (e) {}
+			iLog.info("${joinPoint.getSignature()} executed in ${executionTime}ms");
+				
+			}else{
+		   //If log member does not exist use aspect logging code
+			Logger ilog = log
+			   long start = System.currentTimeMillis();
+			long executionTime = System.currentTimeMillis() - start;
+			log.info("${joinPoint.getSignature()} executed in ${executionTime}ms");
 
-		iLog.info("${joinPoint.getSignature()} executed in ${executionTime}ms");
-		return proceed;
 	}
-}
+			} catch (e) {
+				e.printStackTrace();
+			}
+			
+			//log.info("${joinPoint.getSignature()} executed in ${executionTime}ms");
+			return proceed;
+		 }
+		}
+
