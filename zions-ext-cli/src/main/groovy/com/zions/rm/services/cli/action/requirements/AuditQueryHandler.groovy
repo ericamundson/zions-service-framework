70f9f75ef205edad9097e1561f66d7429615846c
June Boston <june.boston@zionsbancorp.com>
2019-06-12 14:40:08 -0600

    added audit phase to upload items that previously failed to upload
    does essentially the same as the requirements phase, but only for items
    that do not have a wiData object for them.  Is useful as a cleanup.
    
    #180590

---
+++ zions-ext-cli/src/main/groovy/com/zions/rm/services/cli/action/requirements/AuditQueryHandler.groovy
@@ -0,0 +1,10 @@
+package com.zions.rm.services.cli.action.requirements;
+
+import groovy.util.logging.Slf4j
+import org.springframework.stereotype.Component
+
+@Slf4j
+@Component
+public class AuditQueryHandler extends BaseDatabaseQueryHandler {
+
+}
