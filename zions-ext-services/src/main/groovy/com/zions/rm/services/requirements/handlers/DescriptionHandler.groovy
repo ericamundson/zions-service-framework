55c51b1e708516a8c4c39c1ef69d7aae46f0d898
Robert Huet <robert.huet@zionsbancorp.com>
2019-06-13 05:48:22 -0600

    #179618 - Fixed issue where uploaded images in Rational have no extension

--- zions-ext-services/src/main/groovy/com/zions/rm/services/requirements/handlers/DescriptionHandler.groovy
+++ zions-ext-services/src/main/groovy/com/zions/rm/services/requirements/handlers/DescriptionHandler.groovy
@@ -506,6 +506,7 @@ class DescriptionHandler extends RmBaseAttributeHandler {
 	}
 
 	boolean isImageFile(String filename) {
-		return (filename.toLowerCase().indexOf('.png') > 0 || filename.toLowerCase().indexOf('.jpg') > 0)
+		return (filename.toLowerCase().indexOf('.png') > -1 || filename.toLowerCase().indexOf('.jpg') > -1 || 
+				filename.toLowerCase().indexOf('.jpeg') > -1 || (filename.indexOf('.') == -1 && filename.indexOf('Image') > -1))
 	}
 }
