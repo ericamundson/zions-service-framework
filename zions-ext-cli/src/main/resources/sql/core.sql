c4413fcc33d386bbfeb46ec0ccb1ea0bf555b561
Robert Huet <robert.huet@zionsbancorp.com>
2019-09-04 13:40:01 -0600

    #403949 - Updated SQL filter to get all base artifacts

--- zions-ext-cli/src/main/resources/sql/core.sql
+++ zions-ext-cli/src/main/resources/sql/core.sql
@@ -7,7 +7,7 @@ ON T2.REQUIREMENT_ID=T1.REQUIREMENT_ID AND T2.NAME='Release'
 WHERE T1.PROJECT_ID = 19  AND
 (  T1.REQUIREMENT_TYPE NOT IN ( 'Change Request','Actor','Use Case','Subtopic','Formal Use Case (Text)','User Story','Spec Proxy','Function Point','Process Inventory','Term','Use Case Diagram' ) AND
   T2.LITERAL_NAME = 'Deposits'  AND
-  LENGTH(T1.URL) = 65 AND
+  (SUBSTR(T1.URL,43,3) = 'CA_' OR LENGTH(T1.URL) = 65) AND
   TRUNC(T1.REC_DATETIME) <= TO_DATE(:endDate,'mm/dd/yyyy')
 ) AND
 T1.ISSOFTDELETED = 0 AND
