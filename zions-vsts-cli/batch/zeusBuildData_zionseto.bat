697742a3aaaffddb1e949b4c0fcd72218c8b1dba
z091182 <eric.amundson2@zionsbancorp.com>
2019-09-23 12:38:37 -0600

    Fix batch
    #386569

--- zions-vsts-cli/batch/zeusBuildData_zionseto.bat
+++ zions-vsts-cli/batch/zeusBuildData_zionseto.bat
@@ -1,1 +1,1 @@
-java -Dspring.profiles.active=default -jar ..\libs\zions-vsts-cli.jar zeusBuildData  --tfs.url=https://dev.azure.com/zionseto --tfs.user=%ADO_USER% --tfs.token=%ADO_TOKEN% --tfs.project="Zeus" %*
+java -Dspring.profiles.active=default -jar ..\libs\zions-vsts-cli.jar zeusBuildData --tfs.url=https://dev.azure.com/zionseto --tfs.project="Zeus" %*
