b5e26b61e054155a9c1f921bef87ce8b38f6511d
z091182 <eric.amundson2@zionsbancorp.com>
2019-09-26 11:18:33 -0600

    Opps booboo in unit test
    #477823

--- zions-common/src/test/groovy/com/zions/common/services/restart/RestartManagementServiceSpec.groovy
+++ zions-common/src/test/groovy/com/zions/common/services/restart/RestartManagementServiceSpec.groovy
@@ -32,8 +32,8 @@ class RestartManagementServiceSpec extends Specification {
 		Checkpoint cp = new Checkpoint([checkpointId: 0, pageUrl: 'http://theUrl', phase: 'test', logEntries:[]])
 		1 * checkpointManagementService.selectCheckpoint(_) >> cp
 		
-		and: 'stub checkpoint management service addCheckpoint'
-		1 * checkpointManagementService.addCheckpoint(_, _)
+//		and: 'stub checkpoint management service addCheckpoint'
+//		1 * checkpointManagementService.addCheckpoint(_, _)
 
 		and: 'stub query handler getItems'
 		1 * testQueryHandler.getItems() >> [url: 'https://stuff']
