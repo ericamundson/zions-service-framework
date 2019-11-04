3bbd30d41fdf13fe9bd643b0c8ee05f674622660
Robert Huet <robert.huet@zionsbancorp.com>
2019-11-04 14:40:36 -0700

    #565063 - Fix coreR2.sql

--- zions-ext-cli/src/main/resources/sql/coreR2.sql
+++ zions-ext-cli/src/main/resources/sql/coreR2.sql
@@ -6,7 +6,7 @@ LEFT OUTER JOIN RICALM.VW_RQRMENT_ENUMERATION T2
 ON T2.REQUIREMENT_ID=T1.REQUIREMENT_ID AND T2.NAME='Release'
 WHERE T1.PROJECT_ID = 19  AND
 (  T1.REQUIREMENT_TYPE NOT IN ( 'Test Dependency','Formal Use Case (Text)','Technical Design Requirement','Change Request','Actor','Use Case','Subtopic','Formal Use Case (Text)','User Story','Spec Proxy','Function Point','Process Inventory','Term','Use Case Diagram' ) AND
-  T2.LITERAL_NAME IN 'Commercial & Construction Lending','Two DOT One','Two DOT Five','Two DOT Nine')  AND
+  T2.LITERAL_NAME IN ('Commercial ' || chr(38) || ' Construction Lending','Two DOT One','Two DOT Five','Two DOT Nine')  AND
   (SUBSTR(T1.URL,43,3) = 'CA_' OR LENGTH(T1.URL) = 65) AND
   TRUNC(T1.REC_DATETIME) <= TO_DATE(:endDate,'mm/dd/yyyy')
 ) AND
