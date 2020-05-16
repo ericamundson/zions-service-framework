917bbb611670641c78ebcfb8697d23f063473607
Robert Huet <robert.huet@zionsbancorp.com>
2020-05-16 11:20:21 -0600

    #1126302 - More Jama extract stuff

--- zions-ext-cli/src/main/groovy/com/zions/jama/services/cli/action/requirements/TranslateJamaModulesToADO.groovy
+++ zions-ext-cli/src/main/groovy/com/zions/jama/services/cli/action/requirements/TranslateJamaModulesToADO.groovy
@@ -10,6 +10,7 @@ import com.zions.common.services.query.IFilter
 import com.zions.qm.services.metadata.QmMetadataManagementService
 import com.zions.qm.services.test.ClmTestAttachmentManagementService
 import com.zions.jama.services.requirements.JamaRequirementsManagementService
+import com.zions.jama.services.requirements.JamaRequirementsItemManagementService
 import com.zions.vsts.services.admin.member.MemberManagementService
 import com.zions.vsts.services.mr.SmartDocManagementService
 import com.zions.vsts.services.work.FileManagementService
@@ -34,6 +35,8 @@ class TranslateJamaModulesToADO implements CliAction {
 	@Autowired
 	JamaRequirementsManagementService jamaRequirementsManagementService;
 	@Autowired
+	JamaRequirementsItemManagementService jamaRequirementsItemManagementService;
+	@Autowired
 	WorkManagementService workManagementService
 	@Autowired
 	SmartDocManagementService smartDocManagementService
@@ -51,16 +54,116 @@ class TranslateJamaModulesToADO implements CliAction {
 				includes[item] = item
 			}
 		} catch (e) {}
+		String tfsProjectURI = data.getOptionValues('tfs.projectUri')[0]
+		String tfsProject = data.getOptionValues('tfs.project')[0]
+		String collection = ""
+		try {
+			collection = data.getOptionValues('tfs.collection')[0]
+		} catch (e) {}
 
 		if (includes['phases'] != null) {
+			log.info('Getting ADO Project Members...')
+			def memberMap = memberManagementService.getProjectMembersMap(collection, tfsProject)
 			def components = jamaRequirementsManagementService.queryComponents()
 			components.data.each { component ->
 				if (component.location.parent.project) { // Only take top level components
-					String json = new JsonBuilder(component).toPrettyString()
-					println(json)
-					def collection = jamaRequirementsManagementService.queryDocument(component.id)
-					def children = collection.data
-					println(children.size())
+//					String json = new JsonBuilder(component).toPrettyString()
+//					println(json)
+					log.info("Retrieving Module: ${component.fields.name} ...")
+					def module = jamaRequirementsManagementService.getJamaDocument(component)
+					log.info("Processing Module: ${module.getTitle()} ...")
+					
+					// Add Module artifact to ChangeList (to create Document)
+					def changes = jamaRequirementsItemManagementService.getChanges(tfsProject, module, memberMap)
+					def aid = module.getCacheID()
+					changes.each { key, val ->
+						String idkey = "${aid}"
+						idMap[count] = idkey
+						changeList.add(val)
+						count++		
+					}
+	/*							
+					// Iterate through all module elements and add them to changeList
+					def ubound = module.orderedArtifacts.size() - 1
+					def lastSection = 0
+					0.upto(ubound, {
+		
+						// If Heading is immediately followed by a type of artifact that should include it's title, 
+						// move Heading title to following artifact and logically delete Heading artifact
+						if (module.orderedArtifacts[it].getIsHeading() && 
+							it < module.orderedArtifacts.size()-1 && 
+							isToMergeWithHeading(module,it+1)) {
+							
+							module.orderedArtifacts[it+1].setTitle(module.orderedArtifacts[it].getTitle())
+							module.orderedArtifacts[it+1].setDepth(module.orderedArtifacts[it].getDepth())
+							module.orderedArtifacts[it].setIsDeleted(true)
+							return  // Skip Heading artifact 
+						}
+						// If simple heading, remove duplicate description
+						else if (module.orderedArtifacts[it].getIsHeading()) {
+							module.orderedArtifacts[it].setDescription('') 
+						}
+						// If Reporting Requirement is in Reporting RRZ (or included in RSZ), do not migrate the artifact
+						else if ((module.getArtifactType() == 'Reporting RRZ' || module.getArtifactType() == 'RSZ Specification') && 
+								  module.orderedArtifacts[it].getFormat() == 'Text' &&
+								 (module.orderedArtifacts[it].getArtifactType() == 'Reporting Requirement'||
+								  module.orderedArtifacts[it].getArtifactType() == 'Reporting RRZ')) {
+							module.orderedArtifacts[it].setIsDeleted(true)
+						}
+						// Do not migrate Subtopic artifacts
+						else if (module.orderedArtifacts[it].getArtifactType() == 'Subtopic') {
+							module.orderedArtifacts[it].setIsMigrating(false)
+						}
+	
+						// Only store first occurrence of an artifact in the module
+						if (!module.orderedArtifacts[it].getIsDuplicate() && module.orderedArtifacts[it].getIsMigrating()) {  
+							changes = clmRequirementsItemManagementService.getChanges(tfsProject, module.orderedArtifacts[it], memberMap)
+							aid = module.orderedArtifacts[it].getCacheID()
+							changes.each { key, val ->
+								String idkey = "${aid}"
+								idMap[count] = idkey
+								changeList.add(val)
+								count++
+							}
+						}
+						else {
+							log.info("Skipping duplicate requirement #${module.orderedArtifacts[it].getID()}")
+						}
+						// Adjust depth to preserve outline numbering
+						if (lastSection > 0 && module.orderedArtifacts[it].getTfsWorkitemType() != 'Section' &&
+							module.orderedArtifacts[it].getTfsWorkitemType() != 'Document' &&
+							module.orderedArtifacts[it].getDepth() <= module.orderedArtifacts[lastSection].getDepth()){ 
+							// For all requirement content under a section, increment the depth if not already incremented so as to preserve outline numbering from DOORS
+							module.orderedArtifacts[it].incrementDepth(1)
+						}					
+						// save index of last section
+						if (module.orderedArtifacts[it].getTfsWorkitemType() == 'Section') {
+							lastSection = it
+						}
+	
+					})
+					
+					// Create/update work items and SmartDoc container in Azure DevOps
+					if (changeList.size() > 0 && errCount == 0) {
+						// Process work item changes in Azure DevOps
+						log.info("${getCurTimestamp()} - Processing work item changes...")
+						workManagementService.batchWIChanges(collection, tfsProject, changeList, idMap)
+						
+						// Create/update the SmartDoc
+						log.info("${getCurTimestamp()} - Creating SmartDoc: ${module.getTitle()}")
+						def result = smartDocManagementService.ensureSmartDoc(module, tfsUrl, collection, tfsCollectionGUID, tfsProject, tfsProjectURI, tfsTeamGUID, mrTemplate, mrFolder)
+						if (result == null) {
+							log.info("SmartDoc API returned null")
+						}
+						else if (result.error != null && result.error.code != "null") {
+							log.info("SmartDoc API failed.  Error code: ${result.error.code}, Error message: ${result.error.message}, Error name: ${result.error.name}")
+						}
+						else {
+							log.info("SmartDoc API succeeded. Result: ${result.result}")
+						}
+	
+					}
+		*/
 				}
 			}
 			log.info("Processing completed")
--- zions-ext-services/src/main/groovy/com/zions/jama/services/requirements/JamaRequirementsManagementService.groovy
+++ zions-ext-services/src/main/groovy/com/zions/jama/services/requirements/JamaRequirementsManagementService.groovy
@@ -8,9 +8,12 @@ import com.zions.common.services.db.DatabaseQueryService
 import com.zions.common.services.db.IDatabaseQueryService
 import com.zions.common.services.link.LinkInfo
 import com.zions.common.services.rest.IGenericRestClient
+import com.zions.rm.services.requirements.ClmRequirementsModule
+import com.zions.rm.services.requirements.ClmModuleElement
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.stereotype.Component
 import groovy.util.slurpersupport.NodeChild
 import groovy.xml.XmlUtil
@@ -23,8 +26,6 @@ import groovy.util.logging.Slf4j
 import java.nio.charset.StandardCharsets
 import java.text.Normalizer
 import org.apache.commons.io.IOUtils
-
-
 
 /**
  * Handles queries into Jama.
@@ -61,13 +62,53 @@ class JamaRequirementsManagementService {
 				headers: [Accept: 'application/json'] );
 		return results
 	}
-	// Get complete document
-	def queryDocument(def componentID) {
-		String uri = this.jamaGenericRestClient.getJamaUrl() + "/rest/latest/items/$componentID/children"
+	// Get item's children
+	def queryItemChildren(def itemID) {
+		String uri = this.jamaGenericRestClient.getJamaUrl() + "/rest/latest/items/$itemID/children"
 		def children = jamaGenericRestClient.get(
 				uri: uri,
 				headers: [Accept: 'application/json'] );
-		return children
+		return children.data
 	}
+	// Get Jama Document (we will stick it in a ClmRequirementsModule structure to facilitate Smart Doc creation
+	def getJamaDocument(def component) {
+		// Create the top-level Jama Document for the Smart Doc
+		Map moduleAttributeMap = [:]
+		def orderedArtifacts = []
+		// Extract module attributes and members
+		String moduleType = "Component"
+		int depth = 1
+		extractAttributes(moduleAttributeMap, component)
+		getChildHierarchy(orderedArtifacts, component, depth)
+		ClmRequirementsModule clmModule = new ClmRequirementsModule(component.fields.name, null, null, moduleType, null, moduleAttributeMap,orderedArtifacts)
+		return clmModule
+	}
+	def extractAttributes(Map attributeMap, def item) {
+		attributeMap.put('id',item.id)
+		attributeMap.put('documentKey',item.documentKey)
+		attributeMap.put('globalId',item.globalId)
+		attributeMap.put('itemType',item.itemType)
+		attributeMap.put('createdDate',item.createdDate)
+		attributeMap.put('modifiedDate',item.modifiedDate)
+		attributeMap.put('createdBy',item.createdBy)
+		item.fields.each { field ->
+			attributeMap.put(field.key, field.value)
+		}
 
+	}
+	def getChildHierarchy(def orderedArtifacts, def item, def depth) {
+		def children = queryItemChildren(item.id)
+		if (children.size() > 0) {
+			depth++
+			// Instantiate element and add to orderedArtifacts
+			children.each { child ->
+				ClmModuleElement artifact = new ClmModuleElement(child.fields.name, depth, '', 'false', '')
+				extractAttributes(artifact.attributeMap, child)
+				orderedArtifacts.add(artifact) 
+				if (child.itemType == 94001 ) { // This is a set that may have children
+					getChildHierarchy(orderedArtifacts, child, depth)
+				}
+			}
+		}
+	}
 }
