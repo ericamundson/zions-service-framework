159eadb89cb017f582e836168b88356d769c3f32
z091182 <Eric.Amundson2@zionsbancorp.com>
2020-07-08 04:27:07 -0600

    dead letter queue.  Two retries.
    #1361309

--- zions-vsts-microservice/src/main/groovy/com/zions/vsts/services/rmq/mixins/MessageReceiverTrait.groovy
+++ zions-vsts-microservice/src/main/groovy/com/zions/vsts/services/rmq/mixins/MessageReceiverTrait.groovy
@@ -3,17 +3,37 @@ package com.zions.vsts.services.rmq.mixins
 import groovy.json.JsonSlurper
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.amqp.core.MessageListener
+import org.springframework.amqp.core.Message
+import org.springframework.amqp.AmqpRejectAndDontRequeueException;
+import org.springframework.beans.factory.annotation.Value
 
-trait MessageReceiverTrait {
+trait MessageReceiverTrait implements MessageListener {
+	@Autowired
+	RabbitTemplate rabbitTemplate
 	
-	void receive(def message) {
+	@Value('${queue.name:}')
+	String queueName
+
+	void onMessage(Message message) {
 		try {
-			def adoData = new JsonSlurper().parseText(message)
+			String mStr = new String(message.body)
+			def adoData = new JsonSlurper().parseText(mStr)
 			processADOData(adoData)
 		} catch (e) {
-			throw e
+			if (!message.getMessageProperties().redelivered) {
+				//throw new AmqpRejectAndDontRequeueException(e.message)
+				throw e
+			} else {
+				putIntoDeadQueue(message)
+			}
 		}
 		//return 'OK'
 	}
+	
+	private void putIntoDeadQueue(Message failedMessage) {
+		this.rabbitTemplate.send('dead-letter-exchange', "${queueName}-dead", failedMessage);
+	}
+	
 	abstract def processADOData(def adoData)
 }
