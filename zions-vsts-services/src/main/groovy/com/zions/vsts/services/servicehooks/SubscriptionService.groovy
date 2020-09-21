67d67cdc051ea4875a8d9b277280e88489c6154c
James Mcnabb <james.mcnabb@zionsbancorp.com>
2020-09-21 09:44:51 -0500

    Changes to SubscriptionService. Added ServiceConnection pipeline executable service

--- zions-vsts-services/src/main/groovy/com/zions/vsts/services/servicehooks/SubscriptionService.groovy
+++ zions-vsts-services/src/main/groovy/com/zions/vsts/services/servicehooks/SubscriptionService.groovy
@@ -34,16 +34,30 @@ class SubscriptionService {
 			uri: "${genericRestClient.getTfsUrl()}/_apis/hooks/subscriptions",
 			query: query
 			)
-		def retVal = null	
+		def retVal = null
 		results.'value'.each { sub ->
 			String projectIdSub = "${sub.publisherInputs.projectId}"
 			String cProjectId = "${project.id}"
-			// TODO: only checking project. probably need to verify other inputs, ie. changedFields, workItemType, etc.
+			// TODO: only checking project. probably need to verify other publisher inputs, ie. changedFields, workItemType, etc.
 			if (projectIdSub == cProjectId) {
-				retVal = sub
-				System.out.println("SubscriptionService::getSubscription -- Found existing web hook subscription for ${subscriptionData.eventType}")
-				log.info("SubscriptionService::getSubscription -- Found existing web hook subscription for ${subscriptionData.eventType} in project ${project.name}")
-				return
+				if ("${sub.status}" != "disabledByUser") {
+					def inputs = subscriptionData.publisherInputs
+					Iterator<?> keys = inputs.keys()
+					while( keys.hasNext() ) {
+						def key = keys.next()
+						if ("${key}" == "projectId") continue
+						System.out.println("SubscriptionService::getSubscription -- Comparing publisherInputs: key ${key}")
+						def inputValue = inputs.get(key)
+						def subValue = sub.publisherInputs.get(key)
+						System.out.println("Input value: ${inputValue} ... subscription value: ${subValue}")
+						if ( sub.publisherInputs.get(key) != inputs.get(key) ) {
+							return null
+						}
+					}
+					retVal = sub
+					System.out.println("SubscriptionService::getSubscription -- Found existing web hook subscription for ${subscriptionData.eventType}")
+					log.info("SubscriptionService::getSubscription -- Found existing web hook subscription for ${subscriptionData.eventType} in project ${project.name}")
+				}
 			}
 		}
 		return retVal
