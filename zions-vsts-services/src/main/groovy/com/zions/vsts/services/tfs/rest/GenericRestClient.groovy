f6b4be767020101e545627025d659de93475ea12
z091182 <Eric.Amundson2@zionsbancorp.com>
2020-10-07 14:01:57 -0600

    ADO error results
    #1361309

--- zions-vsts-services/src/main/groovy/com/zions/vsts/services/tfs/rest/GenericRestClient.groovy
+++ zions-vsts-services/src/main/groovy/com/zions/vsts/services/tfs/rest/GenericRestClient.groovy
@@ -60,7 +60,12 @@ class GenericRestClient extends AGenericRestClient {
 		this.user = user;
 		delegate = new ARESTClient(tfsUrl)
 		delegate.ignoreSSLIssues()
-		delegate.handler.failure = { it }
+		delegate.handler.failure = { resp ->
+			def outputStream = new ByteArrayOutputStream()
+			resp.entity.writeTo(outputStream)
+			def errorMsg = outputStream.toString('utf8')
+			log.error("ADO Http error response:  ${errorMsg}")
+		}
 		setProxy()
 		setCredentials(user, token);
 		//setupTimeouts()
