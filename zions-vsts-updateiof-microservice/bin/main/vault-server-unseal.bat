ac08bac7dd1634c6054747468cec1925356a2ae3
Michael Angelastro <Michael.Angelastro@zionsbancorp.com>
2021-01-30 10:45:48 -0700

    #1537644 updating mapfield ms

---
+++ zions-vsts-updateiof-microservice/bin/main/vault-server-unseal.bat
@@ -0,0 +1,5 @@
+set http_proxy=
+set https_proxy=
+vault operator unseal An42ZheET/4CLj6uILYhoVfq3NM3y2fiRZ3HCL6On+bM
+vault operator unseal vBTCNH4T+rH9iS2RMSA/g38p2V28WoDvjOmckur/Izeq
+vault operator unseal diqj7Ih5DZAuRSx6Ain/jfKHRzKdeaAxU1UuKD+v+uL5
