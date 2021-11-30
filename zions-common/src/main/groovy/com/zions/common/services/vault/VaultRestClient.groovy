c68df61db4f64a57cc6a50b571adf44a83f6a446
z091182 <Eric.Amundson2@zionsbancorp.com>
2021-11-30 07:24:25 -0700

    log issues
    #1941322

--- zions-common/src/main/groovy/com/zions/common/services/vault/VaultRestClient.groovy
+++ zions-common/src/main/groovy/com/zions/common/services/vault/VaultRestClient.groovy
@@ -5,7 +5,10 @@ import org.springframework.stereotype.Component
 import com.zions.common.services.rest.AGenericRestClient
 import com.zions.common.services.rest.ARESTClient
 
+import groovy.util.logging.Slf4j
+
 @Component
+@Slf4j
 class VaultRestClient extends AGenericRestClient {
 	String vaultUrl
 	
@@ -16,7 +19,18 @@ class VaultRestClient extends AGenericRestClient {
 		this.vaultUrl = url
 		delegate = new ARESTClient(vaultUrl)
 		delegate.ignoreSSLIssues()
-		delegate.handler.failure = { it }
+		delegate.handler.failure = { resp ->
+			if (resp.entity) {
+				def outputStream = new ByteArrayOutputStream()
+				resp.entity.writeTo(outputStream)
+				def errorMsg = outputStream.toString('utf8')
+				if (resp.status == 400 || resp.status == 412)
+					log.info("ADO Http ${resp.status} response:  ${errorMsg}")
+				else
+					log.error("ADO Http ${resp.status} error response:  ${errorMsg}")
+			}
+			return resp
+		}
 		setProxy()
 	}
 	
