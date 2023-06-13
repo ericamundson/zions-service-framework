0fd731cbc9aa732661166a52ae20bdd6285bbe75
James McNabb <james.mcnabb@zionsbancorp.com>
2023-06-13 09:31:33 -0500

    Convert to a generic custom status policy  #2427631

--- zions-vsts-services/src/main/groovy/com/zions/vsts/services/policy/PolicyManagementService.groovy
+++ zions-vsts-services/src/main/groovy/com/zions/vsts/services/policy/PolicyManagementService.groovy
@@ -60,17 +60,19 @@ public class PolicyManagementService {
 	private static final String CI_BUILD_TEMPLATE = "build-template-ci"
 	private static final String RELEASE_BUILD_TEMPLATE = "build-template-release"
 	private static final String CI_BUILD_YAML_FILE = "build-yaml-file"
+	private static final String CHECKMARX_STATUS_NAME = "checkmarx"
+	private static final String SAST_STATUS_GENRE = "sast"
+	private static final String SNOWCI_STATUS_NAME = "snowci-check"
 	private static final String WATCHDOG_STATUS_NAME = "unauthorized-changes"
-	private static final String WATCHDOG_STATUS_GENRE = "continuous-integration"
+	private static final String CI_STATUS_GENRE = "continuous-integration"
 	// ADO policy types
 	private static final String BUILD_VALIDATION_POLICY_TYPE = "0609b952-1397-4640-95ec-e00a01b2c241"
 	private static final String MIN_APPROVERS_POLICY_TYPE = "fa4e907d-c16b-4a4c-9dfa-4906e5d171dd"
 	private static final String LINKED_WI_POLICY_TYPE = "40e92b44-2fe1-4dd6-b3d8-74a9c21d0c6e"
 	private static final String COMMENT_RES_POLICY_TYPE = "c6a1889d-b943-4856-b76f-9e46bb6b0df2"
 	private static final String MERGE_STRATEGY_POLICY_TYPE = "fa4e907d-c16b-4a4c-9dfa-4916e5d171ab"
-	private static final String CHECKMARX_STATUS_POLICY_TYPE = "cbdc66da-9728-4af8-aada-9a5a32e4a226"
 	private static final String AUTOMATICALLY_INCLUDED_REVIEWERS_POLICY_TYPE = "fd2167ab-b0be-447a-8ec8-39368250530e"
-	private static final String WATCHDOG_STATUS_POLICY_TYPE = "cbdc66da-9728-4af8-aada-9a5a32e4a226"
+	private static final String CUSTOM_STATUS_POLICY_TYPE = "cbdc66da-9728-4af8-aada-9a5a32e4a226"
 	
 	public PolicyManagementService() {
 	}
@@ -469,7 +471,7 @@ public class PolicyManagementService {
 	
 	def ensureCheckmarxBranchPolicy(String collection, def projectData, def repoData, String branchName) {
 		
-		def policyRes = getBranchPolicy(CHECKMARX_STATUS_POLICY_TYPE, projectData.id, repoData.id, branchName)
+		def policyRes = getBranchPolicy(CUSTOM_STATUS_POLICY_TYPE, projectData.id, repoData.id, branchName, SAST_STATUS_GENRE, CHECKMARX_STATUS_NAME)
 		if (!policyRes) {
 			def scope = [[matchKind: 'Exact', refName: branchName, repositoryId: repoData.id]]
 			def identity = memberManagementService.getIdentity('', 'svc-cloud-vsbuildagent')
@@ -477,14 +479,14 @@ public class PolicyManagementService {
 			if (identity.size() > 0) {				
 				settings = [authorId: "${identity[0].id}", defaultDisplayName: "Checkmarx scan request sent", invalidateOnSourceUpdate: false, policyApplicability: null, statusGenre: 'sast', statusName: 'checkmarx', scope: scope]
 			}
-			def policy = [isBlocking: true, isDeleted: false, isEnabled: true, settings: settings, type: [id: CHECKMARX_STATUS_POLICY_TYPE]]
+			def policy = [isBlocking: true, isDeleted: false, isEnabled: true, settings: settings, type: [id: CUSTOM_STATUS_POLICY_TYPE]]
 			policyRes = createPolicy(collection, projectData, policy)
 		}
 
 	}
 	
 	def deleteCheckmarxBranchPolicy(def projectData, def repoData, String branchName = 'refs/heads/master') {
-		def policyRes = getBranchPolicy(CHECKMARX_STATUS_POLICY_TYPE, projectData.id, repoData.id, branchName)
+		def policyRes = getBranchPolicy(CUSTOM_STATUS_POLICY_TYPE, projectData.id, repoData.id, branchName, SAST_STATUS_GENRE, CHECKMARX_STATUS_NAME)
 		if (policyRes) {
 			def result = genericRestClient.delete(
 				uri: "${policyRes._links.self.href}",
@@ -497,33 +499,41 @@ public class PolicyManagementService {
 		def policyRes
 		def projectData = repoData.project
 		// check for existing policy
-		policyRes = getBranchPolicy(WATCHDOG_STATUS_POLICY_TYPE, projectData.id, repoData.id, branchName)
+		policyRes = getBranchPolicy(CUSTOM_STATUS_POLICY_TYPE, projectData.id, repoData.id, branchName, CI_STATUS_GENRE, WATCHDOG_STATUS_NAME)
 		if (!policyRes) {
-			log.debug("PolicyManagementService::ensureWatchdogBranchPolicy -- ")
+			log.debug("PolicyManagementService::ensureWatchdogBranchPolicy -- Adding custom status policy for ${CI_STATUS_GENRE}/${WATCHDOG_STATUS_NAME}")
 			def policy = [id: -3, isBlocking: true, isDeleted: false, isEnabled: true, revision: 1,
-			    type: [id: WATCHDOG_STATUS_POLICY_TYPE],
-			    settings:[statusName: WATCHDOG_STATUS_NAME, statusGenre: WATCHDOG_STATUS_GENRE, invalidateOnSourceUpdate: true, defaultDisplayName: 'Watchdog status',
+			    type: [id: CUSTOM_STATUS_POLICY_TYPE],
+			    settings:[statusName: WATCHDOG_STATUS_NAME, statusGenre: CI_STATUS_GENRE, invalidateOnSourceUpdate: true, defaultDisplayName: 'Watchdog status',
 					scope:[[matchKind: 'Exact', refName: branchName, repositoryId: repoData.id]]
 				]
 			]
 			policyRes = createPolicy(collection, projectData, policy)
-		} else {
-			boolean valuesChanged = false
-			if (policyRes.settings.minimumApproverCount != numMinApprovers) {
-				policyRes.settings.minimumApproverCount = numMinApprovers
-				valuesChanged = true
-			}
-			if (policyRes.settings.creatorVoteCounts != creatorVoteCounts) {
-				policyRes.settings.creatorVoteCounts = creatorVoteCounts
-				valuesChanged = true
-			}
-			// update policy for minimum approvers if changes were made
-			if (valuesChanged) {
-				policyRes = updatePolicy(collection, projectData, policyRes)
-			}
 		}
 
 		log.debug("PolicyManagementService::ensureWatchdogBranchPolicy -- result = "+policyRes)
+
+		return policyRes
+	}
+	
+	public def ensureSNowCIBranchPolicy(def collection, def repoData, String branchName = 'refs/heads/master') {
+		def policyRes
+		def projectData = repoData.project
+		// check for existing policy
+			log.info("Checking for existing custom status policy for ${CI_STATUS_GENRE}/${SNOWCI_STATUS_NAME}")
+		policyRes = getBranchPolicy(CUSTOM_STATUS_POLICY_TYPE, projectData.id, repoData.id, branchName, CI_STATUS_GENRE, SNOWCI_STATUS_NAME)
+		if (!policyRes) {
+			log.info("PolicyManagementService::ensureSNowCIBranchPolicy -- Adding custom status policy for ${CI_STATUS_GENRE}/${SNOWCI_STATUS_NAME}")
+			def policy = [id: -3, isBlocking: true, isDeleted: false, isEnabled: true, revision: 1,
+			    type: [id: CUSTOM_STATUS_POLICY_TYPE],
+			    settings:[statusName: SNOWCI_STATUS_NAME, statusGenre: CI_STATUS_GENRE, invalidateOnSourceUpdate: true, defaultDisplayName: 'SNow CI status',
+					scope:[[matchKind: 'Exact', refName: branchName, repositoryId: repoData.id]]
+				]
+			]
+			policyRes = createPolicy(collection, projectData, policy)
+		}
+
+		log.debug("PolicyManagementService::ensureSNowCIBranchPolicy -- result = "+policyRes)
 
 		return policyRes
 	}
@@ -788,7 +798,7 @@ public class PolicyManagementService {
 		return result
 	}
 
-	def getBranchPolicy( def policyType, def projectId, def repoId, def branchName ) {
+	def getBranchPolicy( def policyType, def projectId, def repoId, def branchName, def statusGenre=null, def statusName=null ) {
 		def query = ['repositoryId':"${repoId}", 'refName':"${branchName}" , 'policyType':"${policyType}" ]
 		def results = genericRestClient.get(
 			contentType: ContentType.JSON,
@@ -800,10 +810,15 @@ public class PolicyManagementService {
 		results.'value'.each { policy ->
 			// Need to verify isEnabled
 			if (policy.isEnabled) {
-				retVal = policy
-				System.out.println("PolicyManagementService::getBranchPolicy -- Found existing branch policy for ${policyType}")
-				log.info("PolicyManagementService::getBranchPolicy -- Found existing branch policy for ${policyType}")
-				return
+				if (policyType == CUSTOM_STATUS_POLICY_TYPE) {
+					if (policy.settings.statusName == statusName && policy.settings.statusGenre == statusGenre) {
+						retVal = policy
+						log.info("PolicyManagementService::getBranchPolicy -- Found existing custom status policy for ${statusGenre}/${statusName}")
+					}
+				} else {
+					retVal = policy
+					log.info("PolicyManagementService::getBranchPolicy -- Found existing branch policy for ${policyType}")
+				}
 			}
 		}
 		return retVal
